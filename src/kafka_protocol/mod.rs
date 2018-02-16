376105e11d358ded8a69df705ddc98f61c807814
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-02-17 00:44:58 +1100

    changed to `to_protocol_bytes` with an ownership, not a borrow

--- src/kafka_protocol/mod.rs
+++ src/kafka_protocol/mod.rs
@@ -25,15 +25,15 @@ pub struct ProtocolArray<T> {
 /// Kafka broker.
 ///
 trait ProtocolSerializable {
-    fn to_protocol_bytes(&self) -> ProtocolSerializeResult;
+    fn into_protocol_bytes(self) -> ProtocolSerializeResult;
 }
 
-pub type ProtocolSerializeResult<'a> = Result<Vec<u8>, ProtocolSerializeError>;
+pub type ProtocolSerializeResult = Result<Vec<u8>, ProtocolSerializeError>;
 pub struct ProtocolSerializeError(String);
 
 impl ProtocolSerializable for String {
-    fn to_protocol_bytes(&self) -> ProtocolSerializeResult {
-        I16(self.len() as i16).to_protocol_bytes().and_then(|ref mut string_size|{
+    fn into_protocol_bytes(self) -> ProtocolSerializeResult {
+        I16(self.len() as i16).into_protocol_bytes().and_then(|mut string_size|{
             let mut string_bytes = self.as_bytes().to_vec();
             string_size.append(&mut string_bytes);
             Ok(string_size.clone())
@@ -42,13 +42,13 @@ impl ProtocolSerializable for String {
 }
 
 impl ProtocolSerializable for ProtocolPrimitives {
-    fn to_protocol_bytes(&self) -> ProtocolSerializeResult {
+    fn into_protocol_bytes(self) -> ProtocolSerializeResult {
         let mut payload: Vec<u8> = vec![];
         let serialized =
             match self {
-                &I8(i) => payload.write_i8(i),
-                &I16(i) => payload.write_i16::<BigEndian>(i),
-                &I32(i) => payload.write_i32::<BigEndian>(i)
+                I8(i) => payload.write_i8(i),
+                I16(i) => payload.write_i16::<BigEndian>(i),
+                I32(i) => payload.write_i32::<BigEndian>(i)
             };
         serialized
             .map_err(|e| ProtocolSerializeError(String::from(e.description())))
@@ -58,8 +58,20 @@ impl ProtocolSerializable for ProtocolPrimitives {
 
 impl<T> ProtocolSerializable for ProtocolArray<T>
     where T: ProtocolSerializable {
-    fn to_protocol_bytes(&self) -> ProtocolSerializeResult {
-        // TODO size + byte array
+    fn into_protocol_bytes(self) -> ProtocolSerializeResult {
+        let sequenced =
+            self.array.into_iter().map(|t| {
+                t.into_protocol_bytes()
+            }).collect::<Result<Vec<Vec<u8>>, ProtocolSerializeError>>();
+
+        let array_in_bytes =
+            sequenced.map(|ref s| {
+                s.iter().fold(vec![], |mut acc: Vec<u8>, v| {
+                    acc.append(&mut v.clone());
+                    acc
+                })
+            });
+
         unimplemented!()
     }
 }
@@ -72,10 +84,12 @@ pub struct Request {
 }
 
 impl ProtocolSerializable for Request {
-    fn to_protocol_bytes(&self) -> ProtocolSerializeResult {
+    fn into_protocol_bytes(self) -> ProtocolSerializeResult {
+        let header = self.header;
+        let request_message = self.request_message;
         let header_and_request =
-            self.header.to_protocol_bytes().and_then(|ref mut header|
-                match self.request_message.to_protocol_bytes() {
+            header.into_protocol_bytes().and_then(|ref mut header|
+                match request_message.into_protocol_bytes() {
                     Ok(ref mut request_message) => {
                         header.append(request_message);
                         Ok(header.clone())
@@ -85,8 +99,8 @@ impl ProtocolSerializable for Request {
             );
 
         header_and_request.and_then(|ref mut hr|
-            match I32(hr.len() as i32).to_protocol_bytes() {
-                Ok(ref mut message_size) => {
+            match I32(hr.len() as i32).into_protocol_bytes() {
+                Ok(mut message_size) => {
                     message_size.append(hr);
                     Ok(message_size.clone())
                 },
@@ -106,11 +120,11 @@ pub struct RequestHeader {
 }
 
 impl ProtocolSerializable for RequestHeader {
-    fn to_protocol_bytes(&self) -> ProtocolSerializeResult {
-        I16(self.api_key).to_protocol_bytes().and_then(|ref mut api_key| {
-            I16(self.api_version).to_protocol_bytes().and_then(|ref mut api_version| {
-                I32(self.correlation_id).to_protocol_bytes().and_then(|ref mut correlation_id| {
-                    self.client_id.to_protocol_bytes().and_then(|ref mut client_id| {
+    fn into_protocol_bytes(self) -> ProtocolSerializeResult {
+        I16(self.api_key).into_protocol_bytes().and_then(|mut api_key| {
+            I16(self.api_version).into_protocol_bytes().and_then(|ref mut api_version| {
+                I32(self.correlation_id).into_protocol_bytes().and_then(|ref mut correlation_id| {
+                    self.client_id.into_protocol_bytes().and_then(|ref mut client_id| {
                         api_key.append(api_version);
                         api_key.append(correlation_id);
                         api_key.append(client_id);
@@ -129,12 +143,17 @@ pub enum RequestMessage {
 }
 
 impl ProtocolSerializable for RequestMessage {
-    fn to_protocol_bytes(&self) -> ProtocolSerializeResult {
-
+    fn into_protocol_bytes(self) -> ProtocolSerializeResult {
         match self {
-            &MetadataRequest { ref topics } => unimplemented!()
+            MetadataRequest { ref topics } => unimplemented!()
         }
+        unimplemented!()
+    }
+}
 
+impl RequestMessage {
+    fn serialize_metadata_request(topics: &ProtocolArray<String>) -> ProtocolSerializeResult {
+        // TODO
         unimplemented!()
     }
 }
