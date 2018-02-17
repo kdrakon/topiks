659d282334ce515184f128a5e541678157ddc50e
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-02-17 23:33:15 +1100

    fully implemented MetadataRequest

--- src/kafka_protocol/mod.rs
+++ src/kafka_protocol/mod.rs
@@ -12,6 +12,7 @@ pub enum ProtocolPrimitives {
     I8(i8),
     I16(i16),
     I32(i32),
+    Boolean(bool)
 }
 
 /// Wrapper to a vector for convenience with
@@ -36,7 +37,7 @@ impl ProtocolSerializable for String {
         I16(self.len() as i16).into_protocol_bytes().and_then(|mut string_size|{
             let mut string_bytes = self.as_bytes().to_vec();
             string_size.append(&mut string_bytes);
-            Ok(string_size.clone())
+            Ok(string_size)
         })
     }
 }
@@ -48,7 +49,8 @@ impl ProtocolSerializable for ProtocolPrimitives {
             match self {
                 I8(i) => payload.write_i8(i),
                 I16(i) => payload.write_i16::<BigEndian>(i),
-                I32(i) => payload.write_i32::<BigEndian>(i)
+                I32(i) => payload.write_i32::<BigEndian>(i),
+                Boolean(b) => payload.write_i8(if b { 1 } else { 0 })
             };
         serialized
             .map_err(|e| ProtocolSerializeError(String::from(e.description())))
@@ -59,6 +61,10 @@ impl ProtocolSerializable for ProtocolPrimitives {
 impl<T> ProtocolSerializable for ProtocolArray<T>
     where T: ProtocolSerializable {
     fn into_protocol_bytes(self) -> ProtocolSerializeResult {
+
+        let array_length =
+            I32(self.array.len() as i32).into_protocol_bytes();
+
         let sequenced =
             self.array.into_iter().map(|t| {
                 t.into_protocol_bytes()
@@ -66,13 +72,18 @@ impl<T> ProtocolSerializable for ProtocolArray<T>
 
         let array_in_bytes =
             sequenced.map(|ref s| {
-                s.iter().fold(vec![], |mut acc: Vec<u8>, v| {
+                s.into_iter().fold(vec![] as Vec<u8>, |mut acc: Vec<u8>, v| {
                     acc.append(&mut v.clone());
                     acc
                 })
             });
 
-        unimplemented!()
+        array_length.and_then(|mut payload|{
+            array_in_bytes.map(|ref mut aib|{
+                payload.append(aib);
+                payload
+            })
+        })
     }
 }
 
@@ -132,21 +143,24 @@ impl ProtocolSerializable for RequestHeader {
 /// Enum of requests that can be sent to the Kafka API
 ///
 pub enum RequestMessage {
-    MetadataRequest { topics: ProtocolArray<String> }
+    MetadataRequest { topics: ProtocolArray<String>, allow_auto_topic_creation: bool }
 }
 
 impl ProtocolSerializable for RequestMessage {
     fn into_protocol_bytes(self) -> ProtocolSerializeResult {
         match self {
-            MetadataRequest { ref topics } => unimplemented!()
+            MetadataRequest { topics, allow_auto_topic_creation } => RequestMessage::serialize_metadata_request(topics, allow_auto_topic_creation)
         }
-        unimplemented!()
     }
 }
 
 impl RequestMessage {
-    fn serialize_metadata_request(topics: &ProtocolArray<String>) -> ProtocolSerializeResult {
-        // TODO
-        unimplemented!()
+    fn serialize_metadata_request(topics: ProtocolArray<String>, allow_auto_topic_creation: bool) -> ProtocolSerializeResult {
+        topics.into_protocol_bytes().and_then(|mut t|{
+            Boolean(allow_auto_topic_creation).into_protocol_bytes().map(|ref mut a|{
+                t.append(a);
+                t
+            })
+        })
     }
 }
