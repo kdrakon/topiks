594dd6ff61f070bca3b12b0e8e8a4cff45f2110f
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-03-04 11:58:24 +1100

    reading kafka broker message in entirety

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+extern crate byteorder;
+
 use kafka_protocol::*;
 use kafka_protocol::protocol_primitives::*;
 use kafka_protocol::protocol_serializable::*;
+use self::byteorder::{BigEndian, ReadBytesExt};
 use std::io::*;
 use std::net::TcpStream;
 
@@ -25,15 +28,19 @@ fn main() {
     let send_result =
         TcpStream::connect("localhost:9092").and_then(|mut stream| {
             stream.write(bytes.as_slice()).and_then(|_| {
-                let mut buffer: [u8; 1024] = [0; 1024];
-                stream.read(&mut buffer).map(|result_size| (buffer, result_size))
+                let mut result_size_buf: [u8; 4] = [0; 4];
+                stream.read(&mut result_size_buf).and_then(|_| {
+                    Cursor::new(result_size_buf.to_vec()).read_i32::<BigEndian>()
+                }).and_then(|result_size| {
+                    let mut message_buf: Vec<u8> = vec![0; result_size as usize];
+                    stream.read_exact(&mut message_buf).map(|_| message_buf)
+                })
             })
         });
 
     match send_result {
-        Ok((buffer, size)) => {
+        Ok(result) => {
             println!("printing results...");
-            let (result, _) = buffer.split_at(size);
             for b in result.iter() {
                 print!("{:02X} ", b)
             }
