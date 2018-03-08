d3bea12fafd44663340a390f1fd2c1c53cb2ba04
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-03-09 07:53:39 +1100

    started work on deserializing the MetadataResponse

---
+++ src/kafka_protocol/protocol_response.rs
@@ -0,0 +1,88 @@
+extern crate byteorder;
+
+use self::byteorder::{BigEndian, ReadBytesExt};
+use std::io::*;
+use std::error::Error;
+use super::protocol_primitives::*;
+use super::protocol_serializable::*;
+
+/// Top-level response which can be sent from a Kafka broker.
+///
+pub struct Response<T> {
+    pub header: ResponseHeader,
+    pub response_message: T,
+}
+
+/// Header information for a Response
+///
+pub struct ResponseHeader {
+    pub correlation_id: i32
+}
+
+impl ProtocolDeserializable<ResponseHeader> for Vec<u8> {
+    fn into_protocol_type(self) -> ProtocolDeserializeResult<ResponseHeader> {
+        de_i32(self).map(|correlation_id| {
+            ResponseHeader { correlation_id }
+        }).map_err(|e| ProtocolDeserializeError(String::from(e.description())))
+    }
+}
+
+/// MetadataResponse
+///
+pub struct MetadataResponse {
+    throttle_time_ms: i32,
+    brokers: Vec<BrokerMetadata>,
+    cluster_id: Option<String>,
+    controller_id: i32,
+    topic_metadata: Vec<TopicMetadata>,
+}
+
+pub struct BrokerMetadata {
+    node_id: i32,
+    host: String,
+    port: i32,
+    rack: Option<String>,
+}
+
+pub struct TopicMetadata {
+    error_code: i16,
+    topic: String,
+    is_internal: bool,
+    partition_metadata: Vec<PartitionMetadata>,
+}
+
+pub struct PartitionMetadata {
+    error_code: i16,
+    partition: i32,
+    leader: i32,
+    replicas: Vec<i32>,
+    isr: Vec<i32>,
+    offline_replicas: Vec<i32>,
+}
+
+impl ProtocolDeserializable<Response<MetadataResponse>> for Vec<u8> {
+    fn into_protocol_type(self) -> ProtocolDeserializeResult<Response<MetadataResponse>> {
+        let fields: ProtocolDeserializeResult<(ResponseHeader, MetadataResponse)> =
+            self[0..3].to_vec().into_protocol_type().and_then(|header| {
+                self[4..].to_vec().into_protocol_type().map(|response_message| {
+                    (header, response_message)
+                })
+            });
+
+        fields.map(|(header, response_message)| {
+            Response {
+                header,
+                response_message,
+            }
+        })
+    }
+}
+
+impl ProtocolDeserializable<MetadataResponse> for Vec<u8> {
+    fn into_protocol_type(self) -> ProtocolDeserializeResult<MetadataResponse> {
+        de_i32(self[0..3].to_vec()).and_then(|throttle_time_ms| {
+            unimplemented!()
+        });
+        unimplemented!()
+    }
+}
--- src/kafka_protocol/protocol_serializable.rs
+++ src/kafka_protocol/protocol_serializable.rs
@@ -1,3 +1,10 @@
+extern crate byteorder;
+
+use super::protocol_response::*;
+use self::byteorder::{BigEndian, ReadBytesExt};
+use std::io::Cursor;
+use std::error::Error;
+
 /// If implemented, a struct/enum can be sent on the wire to a
 /// Kafka broker.
 ///
@@ -5,7 +12,29 @@ pub trait ProtocolSerializable {
     fn into_protocol_bytes(self) -> ProtocolSerializeResult;
 }
 
-pub type ProtocolSerializeResult = Result<Vec<u8>, ProtocolSerializeError>;
+pub type ProtocolSerializeResult = Result<Vec<u8>, ProtocolSerializeError>; // TODO replace Result with Result from std::io, which takes one generic type
 
 #[derive(Debug)]
 pub struct ProtocolSerializeError(pub String);
+
+/// If implemented, a Vec<u8> can be read from a Kafka broker
+/// into a type T
+///
+pub trait ProtocolDeserializable<T> {
+    fn into_protocol_type(self) -> ProtocolDeserializeResult<T>;
+}
+
+pub type ProtocolDeserializeResult<T> = Result<T, ProtocolDeserializeError>; // TODO replace Result with Result from std::io, which takes one generic type
+
+#[derive(Debug)]
+pub struct ProtocolDeserializeError(pub String);
+
+// Deserializer Functions
+pub fn de_i32(bytes: Vec<u8>) -> ProtocolDeserializeResult<i32> {
+    Cursor::new(bytes).read_i32::<BigEndian>().map_err(|e| ProtocolDeserializeError(String::from(e.description())))
+}
+
+pub fn de_array(bytes: Vec<u8>) -> ProtocolDeserializeResult<Vec<u8>> {
+    // TODO
+}
+
