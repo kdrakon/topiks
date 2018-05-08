abf0c4b7a9b9b6634a80b91410a77f487dd7b57c
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-05-09 07:14:45 +1000

    dropping cursive for termion
    the callbacks for cursive only allow functions and do not work with
    closures, making much of what I want to do not possible.

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,11 +1,13 @@
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 pub struct AppConfig<'a> {
-    pub bootstrap_server: &'a str
+    pub bootstrap_server: &'a str,
+    pub request_timeout_ms: i32
 }
 
 pub fn from<'a>(args: &'a Vec<String>) -> AppConfig<'a> {
     AppConfig {
-        bootstrap_server: args.get(1).expect("Missing bootstrap server URL")
+        bootstrap_server: args.get(1).expect("Missing bootstrap server URL"),
+        request_timeout_ms: 30000
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,162 +1,38 @@
 extern crate byteorder;
-extern crate cursive;
 #[macro_use]
 extern crate proptest;
+extern crate termion;
 
 use app_config::AppConfig;
-use cursive::align::*;
-use cursive::Cursive;
-use cursive::event::*;
-use cursive::theme::*;
-use cursive::view::Identifiable;
-use cursive::views::*;
+use event_bus::Message;
+use event_bus::BootstrapServer;
 use kafka_protocol::protocol_request::*;
-use kafka_protocol::protocol_requests::metadata_request::*;
 use kafka_protocol::protocol_requests::deletetopics_request::*;
+use kafka_protocol::protocol_requests::metadata_request::*;
 use kafka_protocol::protocol_response::*;
-use kafka_protocol::protocol_responses::metadata_response::*;
 use kafka_protocol::protocol_responses::deletetopics_response::*;
+use kafka_protocol::protocol_responses::metadata_response::*;
+use kafka_protocol::protocol_responses::metadata_response::MetadataResponse;
 use kafka_protocol::protocol_serializable::*;
 use std::env;
+use std::io;
+use std::sync::mpsc::Sender;
+use termion::{color, style};
 
 pub mod kafka_protocol;
 pub mod tcp_stream_util;
 pub mod app_config;
+pub mod event_bus;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let app_config = app_config::from(&args);
+    let sender = event_bus::start();
 
-    let metadata_request = Request::of(MetadataRequest { topics: None, allow_auto_topic_creation: false }, 3, 5);
-    let response: Response<MetadataResponse> =
-        tcp_stream_util::request(app_config.bootstrap_server, metadata_request, |bytes| { bytes.into_protocol_type() }).unwrap();
+    sender.send(Message::GetTopics(BootstrapServer(String::from(app_config.bootstrap_server))));
 
-    let mut cursive = Cursive::new();
-
-    cursive.set_theme(Theme { shadow: false, borders: BorderStyle::Simple, palette: default_palette() });
-    cursive.add_global_callback('q', |c| c.quit());
-
-    let brokers =
-        broker_metadata_textview(response.response_message.cluster_id, response.response_message.controller_id, response.response_message.brokers)
-            .align(Align::new(HAlign::Right, VAlign::Center));
-
-    let topics =
-        OnEventView::new(topic_metadata_selectview(response.response_message.topic_metadata).with_id("topics"))
-            .on_pre_event('d', delete_topic_callback(&app_config));
-
-    let mut info_view = StackView::new();
-    info_view.add_fullscreen_layer(BoxView::with_full_screen(TextView::new("Topiks")));
-
-    let root_layout =
-        LinearLayout::vertical()
-            .child(Panel::new(brokers))
-            .child(LinearLayout::horizontal()
-                .child(BoxView::with_full_screen(topics))
-                .child(Panel::new(info_view.with_id("info_view")))
-            );
-
-    cursive.add_fullscreen_layer(root_layout);
-    cursive.run();
+    std::thread::sleep_ms(5000);
 }
 
-fn broker_metadata_textview(cluster_id: Option<String>, controller_id: i32, brokers: Vec<BrokerMetadata>) -> TextView {
-    let cluster =
-        format!("Cluster: {} Controller ID: {}\n", cluster_id.unwrap_or(String::from("n/a")), controller_id);
 
-    let brokers =
-        brokers.into_iter().map(|metadata| {
-            format!(" {} [{}:{} rack:{}]", metadata.node_id, metadata.host, metadata.port, metadata.rack.unwrap_or(String::from("None")))
-        }).fold(String::new(), |acc, s| {
-            format!("{}{}", acc, s)
-        });
-
-    TextView::new(format!("{} {}", cluster, brokers))
-}
-
-fn topic_metadata_selectview(topic_metadatas: Vec<TopicMetadata>) -> SelectView<TopicMetadata> {
-    let mut topics = SelectView::<TopicMetadata>::new().on_select(topics_select_callback);
-
-    topic_metadatas.into_iter().for_each(|topic_metadata| {
-        topics.add_item(topic_metadata.topic.clone(), topic_metadata)
-    });
-
-    topics
-}
-
-fn topics_select_callback(cursive: &mut Cursive, topic_metadata: &TopicMetadata) {
-    let topic_header =
-        TextView::new(format!("Topic: {}\nPartitions: {} | Internal: {}", topic_metadata.topic.clone(), topic_metadata.partition_metadata.len(), topic_metadata.is_internal));
-
-    let mut partitions = topic_metadata.partition_metadata.clone();
-    partitions.sort_by_key(|e| e.partition);
-
-    let header = format!("{:9} | {:9} | {:9} | {:9} | {:9}", "partition", "leader", "replicas", "isr", "offline replicas");
-    let partition_data = TextView::new(
-        partitions.iter().map(|partition| {
-            let vec_to_string: fn(String, &i32) -> String = |acc, r| { format!("{} {}", acc, r) };
-            let replicas = partition.replicas.iter().fold(String::new(), vec_to_string);
-            let isr = partition.isr.iter().fold(String::new(), vec_to_string);
-            let offline_replicas = partition.offline_replicas.iter().fold(String::new(), vec_to_string);
-            format!("{:9} | {:9} | {:9} | {:9} | {:9}", partition.partition, partition.leader, replicas, isr, offline_replicas)
-        }).fold(header, |lines, line| {
-            format!("{}\n{}", lines, line)
-        })
-    );
-
-    let topic_metadata_layout =
-        LinearLayout::vertical()
-            .child(Panel::new(topic_header))
-            .child(partition_data);
-
-    cursive.call_on_id("info_view", |topic_info: &mut StackView| {
-        topic_info.pop_layer();
-        topic_info.add_fullscreen_layer(BoxView::with_full_screen(topic_metadata_layout));
-    });
-}
-
-fn delete_topic_callback(app_config: &AppConfig) -> fn(&mut Cursive) -> () {
-    |cursive: &mut Cursive| {
-        let delete_verification_view = |topic: &str| {
-            let (head, tail) =
-                match topic.len() {
-                    len if len <= 3 => ("", topic),
-                    len => (&topic[0..len - 3], &topic[len - 3..])
-                };
-
-            let verification = move |accept: String| {
-                OnEventView::new(TextArea::new()).on_pre_event_inner(Key::Enter, move |textarea| {
-                    if textarea.get_content().eq(accept.as_str()) {
-                        println!("deleting");
-                        Some(EventResult::Consumed(None))
-                    } else {
-                        textarea.set_content("");
-                        Some(EventResult::Consumed(None))
-                    }
-                })
-            };
-
-            LinearLayout::horizontal()
-                .child(TextView::new(head))
-                .child(verification(String::from(tail)))
-        };
-
-        let delete_dialog =
-            cursive.call_on_id("topics", |topics: &mut SelectView<TopicMetadata>| {
-                topics.selected_id().and_then(|id| {
-                    topics.get_item(id).map(|(topic, _topic_metadata)| {
-                        Dialog::around(
-                            LinearLayout::vertical()
-                                .child(TextView::new("Delete Topic? Complete the name:"))
-                                .child(delete_verification_view(topic)))
-                            .title("Delete Topic")
-                            .button("Cancel", |cursive| cursive.pop_layer())
-                    })
-                })
-            });
-
-        if let Some(Some(dialog)) = delete_dialog {
-            cursive.add_layer(dialog);
-        }
-    }
-}
 
