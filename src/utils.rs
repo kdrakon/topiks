ff7a1f9c8fa803083df6755772c67877eb0e60ee
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-05-15 16:31:59 +1000

    more topic info work + switched to using AlternateScreen

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,16 @@
 use std::cmp;
+use termion::screen::AlternateScreen;
+use std::io::Stdout;
+use utils;
 
 pub fn pad_right(input: &String, width: u16) -> String {
     let pad_length = cmp::max(width - input.len() as u16, 0);
     (0..pad_length).map(|i| { String::from(" ") }).fold(input.clone(), |a, b| format!("{}{}", a, b))
+}
+
+pub fn bool_yes_no(b: bool) -> String {
+    match b {
+        true => String::from("Yes"),
+        false => String::from("No")
+    }
 }
