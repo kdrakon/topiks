dbcb29c90b700065c52c240a088ead932c5428ad
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-05-16 21:14:23 +1000

    added DescribeConfigsResponse

---
+++ src/kafka_protocol/protocol_responses/describeconfigs_response.rs
@@ -0,0 +1,64 @@
+use kafka_protocol::protocol_response::Response;
+use kafka_protocol::protocol_serializable::*;
+use kafka_protocol::protocol_response::ResponseHeader;
+
+pub struct DescribeConfigsResponse {
+    pub throttle_time_ms: i32,
+    pub resources: Vec<Resource>,
+}
+
+pub struct Resource {
+    error_code: i16,
+    error_message: Option<String>,
+    resource_type: i8,
+    resource_name: String,
+    config_entries: Vec<ConfigEntry>,
+}
+
+pub struct ConfigEntry {
+    config_name: String,
+    config_value: Option<String>,
+    read_only: bool,
+    config_source: i8,
+    is_sensitive: bool,
+    config_synonyms: Vec<ConfigSynonym>,
+}
+
+pub struct ConfigSynonym {
+    config_name: String,
+    config_value: Option<String>,
+    config_source: i8,
+}
+
+//impl ProtocolDeserializable<Response<DescribeConfigsResponse>> for Vec<u8> {
+//    fn into_protocol_type(self) -> ProtocolDeserializeResult<Response<DescribeConfigsResponse>> {
+//        let fields: ProtocolDeserializeResult<(ResponseHeader, DescribeConfigsResponse)> =
+//            self[0..4].to_vec().into_protocol_type().and_then(|header| {
+//                self[4..].to_vec().into_protocol_type().map(|response_message| {
+//                    (header, response_message)
+//                })
+//            });
+//
+//        fields.map(|(header, response_message)| {
+//            Response {
+//                header,
+//                response_message,
+//            }
+//        })
+//    }
+//}
+
+impl ProtocolDeserializable<DescribeConfigsResponse> for Vec<u8> {
+    fn into_protocol_type(self) -> Result<DescribeConfigsResponse, DeserializeError> {
+        de_i32(self[0..=3].to_vec()).and_then(|throttle_time_ms| {
+            de_array(self[4..].to_vec(), deserialize_resource).map(|(resources, remaining_bytes)| {
+                if !remaining_bytes.is_empty() { panic!("Unexpected bytes deserializing DescribeConfigsResponse") }
+                DescribeConfigsResponse { throttle_time_ms, resources }
+            })
+        })
+    }
+}
+
+fn deserialize_resource(bytes: Vec<u8>) -> ProtocolDeserializeResult<DynamicType<Resource>> {
+    unimplemented!()
+}
