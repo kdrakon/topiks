5dad5db0d6eebb2cc201ad26a8b35bcb24183985
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-06-26 14:00:00 +1000

    added progress bar ui object

---
+++ src/user_interface/offset_progress_bar.rs
@@ -0,0 +1,12 @@
+pub fn new(offset: i64, max_offset: i64, width: i64) -> String {
+    let offset = offset.max(0);
+    let max_offset = max_offset.max(1);
+
+    let acc = |a: String, b: &str| format!("{}{}", a, b);
+
+    let block_count = ((offset as f64 / max_offset as f64) * width as f64) as i64;
+    let blocks = (0..block_count).map(|offset| { "█" }).fold(String::from(""), acc);
+    let spaces = (0..(width - (block_count as i64))).map(|offset| { " " }).fold(String::from(""), acc);
+
+    format!("[{}{}]", blocks, spaces)
+}
