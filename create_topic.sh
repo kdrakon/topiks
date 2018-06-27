f70700a34493ad26c76668e2758874100c85b178
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-06-27 15:11:10 +1000

    adding second broker to development env

--- create_topic.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Creating topic \"$1\""
-
-docker exec topiks_broker_1 \
-    kafka-topics --zookeeper zookeeper:2181 --create --if-not-exists --topic $1 --partitions 16 --replication-factor 1
