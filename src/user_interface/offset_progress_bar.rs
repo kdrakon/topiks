19522cd23047f9c2085ea37645dd3351b4a57d32
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-09-25 20:29:01 +1000

    updating some colours

--- src/user_interface/offset_progress_bar.rs
+++ src/user_interface/offset_progress_bar.rs
@@ -6,7 +6,7 @@ pub fn new(offset: i64, max_offset: i64, width: i64) -> String {
 
     let block_count = ((offset as f64 / max_offset as f64) * width as f64) as i64;
     let blocks = (0..block_count).map(|offset| { "█" }).fold(String::from(""), acc);
-    let spaces = (0..(width - (block_count as i64))).map(|offset| { " " }).fold(String::from(""), acc);
+    let spaces = (0..(width - (block_count as i64))).map(|offset| { "_" }).fold(String::from(""), acc);
 
     format!("[{}{}]", blocks, spaces)
 }
