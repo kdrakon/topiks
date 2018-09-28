b0b82231fa1f6d4638f8dac7274d60425cd8bb3a
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2018-09-28 22:39:45 +1000

    wrapping IO around Result

--- src/util/io.rs
+++ src/util/io.rs
@@ -1,44 +1,71 @@
-type Thunk<A> = Box<Fn() -> A>;
+type Thunk<A, E> = Box<Fn() -> Result<A, E>>;
 
-pub struct IO<A> {
-    thunk: Thunk<A>
+pub struct IO<A, E> {
+    thunk: Thunk<A, E>
 }
 
-impl<'a, A: 'a + 'static> IO<A> {
-    pub fn new(f: Box<Fn() -> A>) -> IO<A> {
+impl<A: 'static, E: 'static> IO<A, E> {
+    pub fn new(f: Box<Fn() -> Result<A, E>>) -> IO<A, E> {
         IO {
             thunk: f
         }
     }
 
-    pub fn unsafe_run(self) -> A {
+    pub fn into_result(self) -> Result<A, E> {
         (self.thunk)()
     }
 
-    pub fn map<'b: 'a + 'static, B: 'b>(self, f: Box<Fn(A) -> B>) -> IO<B> {
+    #[inline]
+    pub fn map<B: 'static>(self, f: Box<Fn(A) -> B>) -> IO<B, E> {
         IO {
-            thunk: Box::new(move || f((self.thunk)()))
+            thunk: Box::new(move || {
+                let run: Result<A, E> = (self.thunk)();
+                match run {
+                    Ok(a) => Ok(f(a)),
+                    Err(e) => Err(e)
+                }
+            })
         }
     }
 
-    pub fn and_then<'b: 'a + 'static, B: 'b>(self, f: Box<Fn(A) -> IO<B>>) -> IO<B> {
+    #[inline]
+    pub fn and_then<B: 'static>(self, f: Box<Fn(A) -> IO<B, E>>) -> IO<B, E> {
         IO {
             thunk: Box::new(move || {
-                let b: IO<B> = f((self.thunk)());
-                b.unsafe_run()
+                let run: Result<A, E> = (self.thunk)();
+                match run {
+                    Ok(a) => {
+                        let io: IO<B, E> = f(a);
+                        io.into_result()
+                    }
+                    Err(e) => Err(e)
+                }
+            })
+        }
+    }
+
+    #[inline]
+    pub fn and_then_result<B: 'static>(self, f: Box<Fn(A) -> Result<B, E>>) -> IO<B, E> {
+        IO {
+            thunk: Box::new(move || {
+                let run: Result<A, E> = (self.thunk)();
+                match run {
+                    Ok(a) => f(a),
+                    Err(e) => Err(e)
+                }
             })
         }
     }
 }
 
-pub fn foo() -> Result<String, i8> {
+pub fn foo() -> Result<String, String> {
     let external_io = Box::new(|| Ok(String::from("coo coo coo")));
-    let io: IO<i32> = IO::new(Box::new(|| 42));
+    let io: IO<i32, String> = IO::new(Box::new(|| Ok(42)));
 
-    let io2: IO<Result<String, i8>> =
+    let io2: IO<String, String> =
         io
             .map(Box::new(|i: i32| i * 2))
             .and_then(Box::new(move |i: i32| IO::new(external_io.clone())));
 
-    io2.unsafe_run()
+    io2.into_result()
 }
