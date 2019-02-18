093506e14def30a1ae9cb47554d500c8e1615be2
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2019-02-18 14:20:45 +1100

    adding test, but imcomplete so far
    Only way I can think of doing this test is to save the byte output from
    a real API call

--- src/event_bus/event_bus_test.rs
+++ src/event_bus/event_bus_test.rs
@@ -1,8 +1,50 @@
+use api_client::ApiClient;
+use api_client::ApiClientProvider;
+use api_client::ApiClientTrait;
+use api_client::TcpRequestError;
 use event_bus;
 use event_bus::*;
+use kafka_protocol::protocol_request::Request;
+use kafka_protocol::protocol_response::Response;
+use kafka_protocol::protocol_response::ResponseHeader;
+use kafka_protocol::protocol_responses::metadata_response::MetadataResponse;
+use kafka_protocol::protocol_responses::offsetfetch_response::OffsetFetchResponse;
+use kafka_protocol::protocol_serializable::DeserializeError;
+use kafka_protocol::protocol_serializable::ProtocolDeserializable;
+use kafka_protocol::protocol_serializable::ProtocolDeserializeResult;
+use kafka_protocol::protocol_serializable::ProtocolSerializable;
+use state::State;
+use std::collections::HashMap;
+use std::net::ToSocketAddrs;
+use util::io::IO;
+
+struct FakeApiClient(HashMap<i16, Vec<u8>>); // ApiKey => Byte Response
+
+impl ApiClientTrait for FakeApiClient {
+    fn request<A, T, U>(&self, address: A, request: Request<T>) -> Result<Response<U>, TcpRequestError> where A: ToSocketAddrs, T: ProtocolSerializable, Vec<u8>: ProtocolDeserializable<Response<U>> {
+        let response = self.0.get(&request.header.api_key).expect("ApiKey response not defined");
+        response.clone().into_protocol_type().map_err(|e| TcpRequestError::of(e.error))
+    }
+}
 
 #[test]
-fn foo() {
+#[ignore] // imcomplete
+fn get_metadata() {
+    let state = State::new();
 
-//    assert_eq!(event_bus::to_event(Message::Quit), Event::Exiting);
+    let test_api_client_provider = Box::new(|| IO::new(Box::new(|| Ok(FakeApiClient({
+        let mut responses: HashMap<i16, Vec<u8>> = HashMap::new();
+        responses.insert(3, vec![0, 2]);
+        responses
+    })))));
+
+    match event_bus::to_event(Message::GetMetadata(BootstrapServer(String::from("fake")), None), test_api_client_provider) {
+        Event::MetadataRetrieved(statefn) => {
+            match statefn(&state) {
+                Ok(metadata_payload) => (),
+                Err(e) => panic!()
+            }
+        }
+        _ => panic!("Expected MetadataRetrieved")
+    }
 }
