2d96f354a9500ede09461fd1366d2604929e693b
Sean Policarpio <sean.policarpio@simplemachines.com.au>
2019-03-21 13:37:41 +1100

    reducing allocs in PagedVec

--- src/util/paged_vec.rs
+++ src/util/paged_vec.rs
@@ -1,14 +1,17 @@
 pub struct PagedVec<'a, A: 'a> {
     page_length: usize,
-    pages: Vec<Vec<&'a A>>,
+    vec: &'a Vec<A>,
 }
 
 impl<'a, A> PagedVec<'a, A> {
     pub fn from(vec: &'a Vec<A>, page_length: usize) -> PagedVec<'a, A> {
-        PagedVec { page_length, pages: vec.chunks(page_length).map(|slice| slice.iter().collect::<Vec<&'a A>>()).collect::<Vec<Vec<&'a A>>>() }
+        PagedVec { page_length, vec }
     }
 
-    pub fn page(&'a self, index: usize) -> Option<(usize, &'a Vec<&'a A>)> {
-        self.pages.get((index as f32 / self.page_length as f32).floor() as usize).map(|page| (index % self.page_length, page))
+    pub fn page(&'a self, index: usize) -> Option<(usize, Vec<&'a A>)> {
+        let mut paged = self.vec.chunks(self.page_length);
+        let opt_page = paged.nth((index as f32 / self.page_length as f32).floor() as usize);
+
+        opt_page.map(|page| (index % self.page_length, page.iter().collect::<Vec<&'a A>>()))
     }
 }
